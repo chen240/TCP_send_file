3b9bc9d95817d445ac3eaa0036487e1418c67258
Tedu <1852283984@qq.com>
2020-04-03 23:44:48 +0800

    Add some files

---
+++ PROJECT1/HTTPServer/HttpServer/setting.py
@@ -0,0 +1,13 @@
+#http server配置文件
+
+#httpserver地址
+HOST='0.0.0.0'
+PORT=8000
+ADDR=(HOST,PORT)
+
+#webframe地址
+frame_ip='127.0.0.1'
+from_port=8080
+frame_addr=(frame_ip,frame_port)
+
+
---
+++ PROJECT1/HTTPServer/WebFrame/views.py
@@ -0,0 +1,10 @@
+# views.py
+from time import ctime
+
+def show_time():
+    return ctime()
+
+def say_hello():
+    return "Hello Python"
+def say_bye():
+    return "Good bye"
